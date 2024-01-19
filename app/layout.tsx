63fdea1d53ca2237a1d595ae018f93a816551816
jonabecher <jonajbecher@gmail.com>
2024-01-19 08:05:30 +0100

    change: use wss instead of ws

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@
 
 import { Inter } from 'next/font/google'
 import '../styles/globals.css'
-import type { AppProps } from "next/app"
 import { QueryClient, QueryClientProvider } from "react-query"
 
 const inter = Inter({ subsets: ['latin'] })
